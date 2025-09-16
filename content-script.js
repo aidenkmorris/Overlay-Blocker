266d480cc03e068d835cb356de113dcbd3bf678e
aiden <aiden@DUBSTEPMACHINE>
2025-09-16 13:41:29 -0400

    Initial commit

---
+++ content-script.js
@@ -0,0 +1,17 @@
+// const top = document.createElement("div");
+// top.id = "top";
+
+// const bottom = document.createElement("div");
+// bottom.id = "bottom";
+
+// const left = document.createElement("div");
+// left.id = "left";
+
+// const right = document.createElement("div");
+// right.id = "right";
+
+// let overlay = document.getElementById("overlay-blocker");
+// overlay.appendChild(top);
+// overlay.appendChild(bottom);
+// overlay.appendChild(left);
+// overlay.appendChild(right);
